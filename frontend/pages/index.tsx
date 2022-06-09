6d28c52ae37f055547c1e2f4e0d3181e16e07747
elmol <eliasmolini@gmail.com>
2022-06-09 19:55:37 -0300

    allow input in question ask by material ui and react hook form

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -6,9 +6,32 @@ import { FormEvent, useEffect } from "react";
 import styles from "../styles/Home.module.css";
 import Game from "../public/Game.json";
 import { GameConnection } from "./game-connection";
+import { Box, Button, Container, createTheme, CssBaseline, Grid, TextField, ThemeProvider } from "@mui/material";
+import { SubmitHandler, useForm } from "react-hook-form";
+import Typography from "@mui/material/Typography";
+
+type Question = {
+  position: string;
+  number: string;
+};
 
 const Home: NextPage = () => {
+  const theme = createTheme();
+
   const gameConnection = new GameConnection();
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset,
+  } = useForm<Question>();
+
+  const onSubmit: SubmitHandler<Question> = (question) => {
+    const position = parseInt(question.position?.trim() ?? "0");
+    const number = parseInt(question.number?.toString()?.trim() ?? "0");
+    gameConnection.askQuestion(position, number);
+  };
 
   async function onInit() {
     const provider = new providers.JsonRpcProvider("http://localhost:8545");
@@ -17,18 +40,56 @@ const Home: NextPage = () => {
     console.log("Game contract address", contract.address);
   }
 
-  function handleAsk(event: FormEvent<HTMLFormElement>) {
-    event.preventDefault();
-    const data = new FormData(event.currentTarget);
-    const position = parseInt(data.get("position")?.toString()?.trim() ?? "0");
-    const number = parseInt(data.get("number")?.toString()?.trim() ?? "0");
-    gameConnection.askQuestion(position, number);
-  }
+  // function handleAsk(event: FormEvent<HTMLFormElement>) {
+  //   event.preventDefault();
+  //   const data = new FormData(event.currentTarget);
+  //   const position = parseInt(data.get("position")?.toString()?.trim() ?? "0");
+  //   const number = parseInt(data.get("number")?.toString()?.trim() ?? "0");
+  //   gameConnection.askQuestion(position, number);
+  // }
 
   useEffect(() => {
     onInit();
   }, []);
 
+  const questionAsk = (
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <Box
+        sx={{
+          marginTop: 8,
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        <Typography component="h1" variant="h5">
+          Ask about the position and number
+        </Typography>
+        <Box component="form" noValidate onSubmit={handleSubmit(onSubmit)} sx={{ mt: 3 }}>
+          <Grid container spacing={2}>
+            <Grid item xs={12} sm={3}>
+              <TextField autoComplete="position" required id="position" label="Position" size="small" {...register("position") } autoFocus />
+            </Grid>
+            <Grid item xs={12} sm={3}>
+              <TextField autoComplete="number" required id="number" label="Number" size="small" {...register("number") } autoFocus />
+            </Grid>
+            <Grid item xs={12} sm={3}>
+              <Button type="submit" fullWidth variant="contained">
+                ask
+              </Button>
+            </Grid>
+
+            <Grid item xs={12} sm={3}>
+              <Button variant="outlined" onClick={() => gameConnection.responseQuestion()}>
+                awk
+              </Button>
+            </Grid>
+          </Grid>
+        </Box>
+      </Box>
+    </Container>
+  );
   return (
     <div className={styles.container}>
       <Head>
@@ -43,19 +104,7 @@ const Home: NextPage = () => {
         <div className={styles.description}>Character 3210 will be selected...</div>
         <button onClick={() => gameConnection.selection()}>Select</button>
 
-        <div className={styles.description}>Ask about the position and number</div>
-        <form
-          onSubmit={(e) => {
-            handleAsk(e);
-          }}
-        >
-          <input id="position" name="position" />
-          <input id="number" name="number" />
-          <input type="submit" value="Ask" />
-        </form>
-
-        <div className={styles.description}>Response 1 for last ask 0,3</div>
-        <button onClick={() => gameConnection.responseQuestion()}>Response Ask</button>
+        <ThemeProvider theme={theme}>{questionAsk}</ThemeProvider>
         <div className={styles.description}>Guess 3210</div>
         <button onClick={() => gameConnection.guess()}>Guess</button>
         <div className={styles.description}>Response 1 for Guess 3210</div>
