2d0d4d9dc910192ea9be9271072d30ad41b1a255
elmol <eliasmolini@gmail.com>
2022-06-29 19:05:52 -0300

    showing network name

--- frontend/components/WalletConnector.tsx
+++ frontend/components/WalletConnector.tsx
@@ -1,13 +1,13 @@
-import Fab from '@mui/material/Fab';
-import Backdrop from '@mui/material/Backdrop';
-import { useEffect, useState } from 'react';
-import { GameConnection } from '../game/game-connection';
+import Fab from "@mui/material/Fab";
+import Backdrop from "@mui/material/Backdrop";
+import { useEffect, useState } from "react";
+import { GameConnection } from "../game/game-connection";
 
 //const testnetChainId = '0x6357d2e0'
-const localChainId = '0x7a69' //31337
-const devnetChainId = '0x635ae020' //1666900000
+const localChainId = "0x7a69"; //31337
+const devnetChainId = "0x635ae020"; //1666900000
 const testnetChainId = devnetChainId; //because is disabled
-const mainnetChainId='0x63564c40' // 1666600000 
+const mainnetChainId = "0x63564c40"; // 1666600000
 
 // const testnetConfig = {
 //   chainId: testnetChainId,
@@ -21,33 +21,45 @@ const mainnetChainId='0x63564c40' // 1666600000
 //   blockExplorerUrls: ['https://explorer.pops.one']
 // };
 
+const localConfig = {
+  chainId: localChainId,
+  chainName: "Local",
+  nativeCurrency: {
+    name: "ONE",
+    symbol: "ONE",
+    decimals: 18,
+  },
+  rpcUrls: ["http://localhost:8545"],
+  blockExplorerUrls: ["http://localhost:8080"],
+};
 
 const devnetConfig = {
   chainId: testnetChainId,
-  chainName: 'Harmony Devnet',
+  chainName: "Harmony Devnet",
   nativeCurrency: {
-    name: 'ONE',
-    symbol: 'ONE',
-    decimals: 18
+    name: "ONE",
+    symbol: "ONE",
+    decimals: 18,
   },
-  rpcUrls: ['https://api.s0.ps.hmny.io'],
-  blockExplorerUrls: ['https://explorer.ps.hmny.io/']
+  rpcUrls: ["https://api.s0.ps.hmny.io"],
+  blockExplorerUrls: ["https://explorer.ps.hmny.io/"],
 };
 
 const mainnetConfig = {
   chainId: mainnetChainId,
-  chainName: 'Harmony Mainnet',
+  chainName: "Harmony Mainnet",
   nativeCurrency: {
-    name: 'ONE',
-    symbol: 'ONE',
-    decimals: 18
+    name: "ONE",
+    symbol: "ONE",
+    decimals: 18,
   },
   // rpcUrls: ['https://api.harmony.one'], // not working
-  rpcUrls: ['https://harmony-mainnet.chainstacklabs.com'],
-  blockExplorerUrls: ['https://explorer.harmony.one/']
+  rpcUrls: ["https://harmony-mainnet.chainstacklabs.com"],
+  blockExplorerUrls: ["https://explorer.harmony.one/"],
 };
 
-const testnetConfig=devnetConfig; //because is disabled
+const testnetConfig = devnetConfig; //because is disabled
+const chains = [localConfig, devnetConfig, mainnetConfig];
 
 interface IMyProps {
   connectionHandle: () => Promise<void>;
@@ -55,11 +67,11 @@ interface IMyProps {
 
 // source code form https://github.com/socathie/zkApp
 export default function WalletConnector(props: IMyProps) {
-  let ethereum:any = undefined;
+  let ethereum: any = undefined;
 
   const [currentAccount, setCurrentAccount] = useState<string | null>(null);
+  const [currentChain, setCurrentChain] = useState<string | null>(null);
   const [correctChain, setCorrectChain] = useState<boolean | null>(null);
-
 
   function init() {
     ethereum = window.ethereum as any;
@@ -75,10 +87,8 @@ export default function WalletConnector(props: IMyProps) {
     }
   }
 
-
   const checkWalletIsConnected = async () => {
-
-    const accounts = await ethereum.request({ method: 'eth_accounts' });
+    const accounts = await ethereum.request({ method: "eth_accounts" });
 
     if (accounts.length !== 0) {
       const account = accounts[0];
@@ -87,63 +97,64 @@ export default function WalletConnector(props: IMyProps) {
     } else {
       console.log("No authorized account found");
     }
-  }
+  };
 
   const connectWalletHandler = async () => {
     try {
-      const accounts = await ethereum.request({ method: 'eth_requestAccounts' });
+      const accounts = await ethereum.request({ method: "eth_requestAccounts" });
       console.log("Found an account! Address: ", accounts[0]);
       setCurrentAccount(accounts[0]);
     } catch (err) {
-      console.log(err)
+      console.log(err);
     }
-  }
+  };
 
   function isValidChain(chainId: any) {
     return chainId === testnetChainId || chainId === localChainId || chainId === devnetChainId || chainId === mainnetChainId;
   }
 
   const checkChainId = async () => {
-    let chainId = await ethereum.request({ method: 'eth_chainId' });
+    let chainId = await ethereum.request({ method: "eth_chainId" });
     console.log("Chain ID:", chainId, parseInt(chainId));
-
     setCorrectChain(isValidChain(chainId));
-  }
+    const chainName = chains.find((chain) => chain.chainId === chainId)?.chainName;
+    setCurrentChain(chainName?chainName:"Unknown");
+  };
 
   const checkConnection = async () => {
-   await checkWalletIsConnected();
-   await checkChainId();
-   if (currentAccount && correctChain) {
-    try {
-      await props.connectionHandle();
-    } catch (err) {
-      console.log(err);
+    await checkWalletIsConnected();
+    await checkChainId();
+    if (currentAccount && correctChain) {
+      try {
+        await props.connectionHandle();
+      } catch (err) {
+        console.log(err);
+      }
     }
-   }
   };
 
   const changeChainId = async () => {
-    let chainId = await ethereum.request({ method: 'eth_chainId' });
+    let chainId = await ethereum.request({ method: "eth_chainId" });
 
     if (!isValidChain(chainId)) {
       try {
         await ethereum.request({
-          method: 'wallet_switchEthereumChain',
-          params: [{
-            chainId: testnetChainId
-          }], // chainId must be in hexadecimal numbers
+          method: "wallet_switchEthereumChain",
+          params: [
+            {
+              chainId: testnetChainId,
+            },
+          ], // chainId must be in hexadecimal numbers
         });
-        chainId = await ethereum.request({ method: 'eth_chainId' });
-      } catch (error:any) {
+        chainId = await ethereum.request({ method: "eth_chainId" });
+      } catch (error: any) {
         // This error code indicates that the chain has not been added to MetaMask
         // if it is not, then install it into the user MetaMask
         if (error.code === 4902) {
           try {
             await ethereum.request({
-              method: 'wallet_addEthereumChain',
-              params: [
-                testnetConfig,
-              ],
+              method: "wallet_addEthereumChain",
+              params: [testnetConfig],
             });
           } catch (addError) {
             console.error(addError);
@@ -155,23 +166,24 @@ export default function WalletConnector(props: IMyProps) {
     }
     console.log(chainId);
     setCorrectChain(isValidChain(chainId));
-  }
+  };
 
   const changeAccount = async () => {
     await ethereum.request({
-      method: 'wallet_requestPermissions',
-      params: [{
-        eth_accounts: {},
-      }]
+      method: "wallet_requestPermissions",
+      params: [
+        {
+          eth_accounts: {},
+        },
+      ],
     });
     window.location.reload();
-  }
+  };
 
   useEffect(() => {
     init();
     checkConnection();
-  })
-
+  });
 
   const ConnectWalletFab = () => {
     return (
@@ -181,31 +193,27 @@ export default function WalletConnector(props: IMyProps) {
           Connect Wallet
         </Fab>
       </div>
-    )
-  }
+    );
+  };
 
   const WrongNetworkFab = () => {
     return (
       <div>
         <Backdrop open={true} />
-        <Fab variant="extended" color="secondary" onClick={changeChainId} >
+        <Fab variant="extended" color="secondary" onClick={changeChainId}>
           Wrong Network
         </Fab>
       </div>
-    )
-  }
+    );
+  };
 
   const AccountFab = () => {
     return (
-      <Fab variant="extended" onClick={changeAccount}>
-        {currentAccount?.slice(0, 8)}...{currentAccount?.slice(-5)}
+      <Fab color="primary" variant="extended" onClick={changeAccount}>
+         {currentChain} | {currentAccount?.slice(0, 8)}...{currentAccount?.slice(-5)}
       </Fab>
-    )
-  }
+    );
+  };
 
-  return (
-    <div>
-      {(currentAccount && correctChain) ? <AccountFab /> : (currentAccount ? <WrongNetworkFab /> : <ConnectWalletFab />)}
-    </div>
-  )
+  return <div>{currentAccount && correctChain ? <AccountFab /> : currentAccount ? <WrongNetworkFab /> : <ConnectWalletFab />}</div>;
 }
